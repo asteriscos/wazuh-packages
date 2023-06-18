3c2a73da68d9bfa4927ccb4337a982b4121fbf08
Federico Rodriguez <federico.rodriguez@wazuh.com>
2023-06-18 15:53:48 +0200

    Change plugin tab title

--- stack/dashboard/plugin/plugin-customization.sh
+++ stack/dashboard/plugin/plugin-customization.sh
@@ -13,15 +13,21 @@ sed -i 's|"Wazuh dashboard"|"Nokia dashboard"|g' ./opensearch-dashboards/wazuh/t
 gzip -c ./opensearch-dashboards/wazuh/target/public/wazuh.plugin.js > ./opensearch-dashboards/wazuh/target/public/wazuh.plugin.js.gz
 brotli -c ./opensearch-dashboards/wazuh/target/public/wazuh.plugin.js > ./opensearch-dashboards/wazuh/target/public/wazuh.plugin.js.br
 
-### Remove Health Check Wazuh name 
 
 # plugins/wazuh/target/public/wazuh.chunk.10.js
 # /home/wazuh/Downloads/packages/wazuh.chunk.10.js
 
+### Remove Health Check Wazuh name 
 sed -i 's|Check Wazuh API connection|Check server API connection|g' ./opensearch-dashboards/wazuh/target/public/wazuh.chunk.10.js
 sed -i 's|Check Wazuh API version|Check server API version|g' ./opensearch-dashboards/wazuh/target/public/wazuh.chunk.10.js
+
 sed -i 's|Wazuh API seems to be down|Server API seems to be down|g' ./opensearch-dashboards/wazuh/target/public/wazuh.chunk.10.js
 sed -i 's|Check the Wazuh API service status|Check the server API service status|g' ./opensearch-dashboards/wazuh/target/public/wazuh.chunk.10.js
+
+### Change tab title and breadcrumb name
+sed -i 's|chrome.docTitle.change("Wazuh".concat(pageTitleSuffix));|chrome.docTitle.change("Dashboards".concat(pageTitleSuffix));|g' ./opensearch-dashboards/wazuh/target/public/wazuh.chunk.10.js
+sed -i 's|"discover.discoverBreadcrumbTitle",{defaultMessage:"Wazuh"}|"discover.discoverBreadcrumbTitle",{defaultMessage:"Dashboards"}|g' ./opensearch-dashboards/wazuh/target/public/wazuh.chunk.10.js
+
 gzip -c ./opensearch-dashboards/wazuh/target/public/wazuh.chunk.10.js > ./opensearch-dashboards/wazuh/target/public/wazuh.chunk.10.js.gz
 brotli -c ./opensearch-dashboards/wazuh/target/public/wazuh.chunk.10.js > ./opensearch-dashboards/wazuh/target/public/wazuh.chunk.10.js.br
 
